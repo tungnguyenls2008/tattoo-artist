9ed12933a1c787ed220080c6b4c584220ecb3750
tungnd <tungnd@vaymuon.vn>
2021-03-05 17:02:32 +0700

    cosmetic changes

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Portfolio</title>
+    <title>Greeny Ink ® 2021</title>
 
     <!-- Favicons -->
     <link href="{{asset('img/favicon.png')}}" rel="icon">
